f59c4168c688dbb071af21958f309a7311932327
sandesh <sandeshsri01@gmail.com>
2026-04-24 12:30:26 +0530

    [Sandesh] Add. Practice problem

--- practice-problem/Program.cs
+++ practice-problem/Program.cs
@@ -1,45 +1,45 @@
-﻿using System;
+﻿//using System;
 
-class EmployeeWage
-{
-    static void Main()
-    {
-        Console.Write("Enter wage per hour: ");
-        int wagePerHour = Convert.ToInt32(Console.ReadLine());
+//class EmployeeWage
+//{
+//    static void Main()
+//    {
+//        Console.Write("Enter wage per hour: ");
+//        int wagePerHour = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter full time hrs ");
-        int fullTime = Convert.ToInt32(Console.ReadLine());
+//        Console.Write("Enter full time hrs ");
+//        int fullTime = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter part time hrs ");
-        int partTime = Convert.ToInt32(Console.ReadLine());
+//        Console.Write("Enter part time hrs ");
+//        int partTime = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter number of working day: ");
-        int days = Convert.ToInt32(Console.ReadLine());
+//        Console.Write("Enter number of working day: ");
+//        int days = Convert.ToInt32(Console.ReadLine());
 
-        int totalHours = 0;
+//        int totalHours = 0;
 
-        Random random = new Random();
+//        Random random = new Random();
 
-        for (int i = 1; i <= days; i++)
-        {
-            int attendance = random.Next(0, 3);
-            int dailyHours = 0;
+//        for (int i = 1; i <= days; i++)
+//        {
+//            int attendance = random.Next(0, 3);
+//            int dailyHours = 0;
 
-            if (attendance == 1)
-                dailyHours = fullTime;
-            else if (attendance == 2)
-                dailyHours = partTime;
-            else
-                dailyHours = 0;
+//            if (attendance == 1)
+//                dailyHours = fullTime;
+//            else if (attendance == 2)
+//                dailyHours = partTime;
+//            else
+//                dailyHours = 0;
 
-            totalHours += dailyHours;
+//            totalHours += dailyHours;
 
-            Console.WriteLine("Day " + i + " -> Hours: " + dailyHours);
-        }
+//            Console.WriteLine("Day " + i + " -> Hours: " + dailyHours);
+//        }
 
-        int totalSalary = totalHours * wagePerHour;
+//        int totalSalary = totalHours * wagePerHour;
 
-        Console.WriteLine("Total Hours: " + totalHours);
-        Console.WriteLine("Total Salary: " + totalSalary);
-    }
-}
+//        Console.WriteLine("Total Hours: " + totalHours);
+//        Console.WriteLine("Total Salary: " + totalSalary);
+//    }
+//}
